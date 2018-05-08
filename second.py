ae70e0c2e57e275efd01fa7c8fd8982b513dd332
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-05-08 17:25:57 +1000

    second factor file

---
+++ second.py
@@ -0,0 +1,7 @@
+firstNumber = int(input("Enter first number: "));
+secondNumber = int(input("Enter second number: "));
+
+if(firstNumber % secondNumber is 0):
+    print(secondNumber," is a factor of ",firstNumber);
+else:
+    print(secondNumber," is not a factor of ",firstNumber);
