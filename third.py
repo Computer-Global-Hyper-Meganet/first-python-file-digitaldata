41218482818b7ba83e576cf4debb0e48c7a4c4b0
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-05-25 21:27:30 +1000

    third

--- third.py
+++ third.py
@@ -1,6 +1,18 @@
-str = input('enter your name: ');
+str = input('enter your full name: ');
+firstName = "";
+lastName = "";
+isLastName = False;
 
 for i in range(len(str)):
-    if(str[i] != " "):
-        print(str[i]);
-    
+    if(str[i] is " "):
+        isLastName = True;
+    else:
+        if(not isLastName):
+            firstName += str[i];
+        else:
+            lastName += str[i];
+
+print('First Name: ' + firstName);
+print('Last Name: ' + lastName);
+
+        
